2741f96f480513e06d05ae7edac7d505f5b42005
Pramodh BR <72683141+Pramodh003@users.noreply.github.com>
2024-01-04 13:38:55 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull pramod003/simple-python-flask
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 pramod003/simple-python-flask
